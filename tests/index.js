59a0aa9d0dd3600bb76e2bc91ec43b479b58dd03
Kobezzza <kobezzza@mail.ru>
2014-10-26 12:09:53 +0400

    Рефакторинг

--- tests/index.js
+++ tests/index.js
@@ -1,13 +1,11 @@
-var __NEJS_THIS__ = this;
 var basePath = 'tests';
-var builder = require('../builder');
+var monic = require('../monic');
 
 var fs = require('fs');
 var path = require('path');
 
 if (process.argv[2]) {
-	builder.compile(path.join(basePath, process.argv[2], 'test.js'), [], {}, function (err, result) {
-		
+	monic.compile(path.join(basePath, process.argv[2], 'test.js'), [], {}, function (err, result) {
 		if (err) {
 			throw err;
 		}
@@ -17,30 +15,25 @@ if (process.argv[2]) {
 
 } else {
 	fs.readdir(basePath, function (err, dirs) {
-		
 		if (err) {
 			throw err;
 		}
 
 		dirs.forEach(function (dir) {
-			
 			var dirPath = path.join(basePath, dir);
 
 			fs.stat(dirPath, function (err, stat) {
-				
 				if (err) {
 					throw err;
 				}
 
 				if (stat.isDirectory()) {
-					builder.compile(path.join(dirPath, 'test.js'), [], {}, function (err, builderResult) {
-						
+					monic.compile(path.join(dirPath, 'test.js'), [], {}, function (err, builderResult) {
 						if (err) {
 							throw err;
 						}
 
 						fs.readFile(path.join(dirPath, 'result.js'), 'utf8', function (err, result) {
-							
 							if (err) {
 								throw err;
 							}
