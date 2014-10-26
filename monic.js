7f2b65120d1e83ddf9afdf2b1bde706195a4fb93
Kobezzza <kobezzza@mail.ru>
2014-10-26 16:30:30 +0400

    Рефакторинг

--- monic.js
+++ monic.js
@@ -4,25 +4,37 @@ var Parser = require('./build/parser');
 exports.VERSION = [1, 0, 0];
 
 /**
- * Компилировать указанный файл
+ * Обработать указанный файл
  *
  * @param {string} file - адрес файла
- * @param {?string} [content] - содержимое файла
- * @param {Array} [labels] - массив меток
- * @param {Object} [flags] - таблица флагов
+ * @param {Object} [params] - дополнительные параметры операции
+ * @param {Array=} [params.flags] - массив заданных флагов
+ * @param {Array=} [params.labels] - массив заданных меток
+ * @param {?string=} [params.content] - текст файла
  * @param {function(Error, string=, string=)} callback - функция обратного вызова
  */
-exports.compile = function(file, content, labels, flags, callback) {
+exports.compile = function(file, params, callback) {
+	params = params || {};
+
+	params.flags = params.flags || [];
+	params.labels = params.labels || [];
+
+	if (params.flags) {
+		params.flags = params.flags.reduce(function (res, el, key) {
+			res[key] = true;
+		}, {});
+	}
+
 	function finish(err, fileStructure, path) {
 		if (err) {
 			return callback(err);
 		}
 
-		callback(null, fileStructure.compile(labels, flags), path);
+		callback(null, fileStructure.compile(params.labels, params.flags), path);
 	}
 
-	if (content) {
-		new Parser().parse(file, content, finish);
+	if (params.content) {
+		new Parser().parse(file, params.content, finish);
 
 	} else {
 		new Parser().parseFile(file, finish);
