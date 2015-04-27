0115b7a16eeccfa21f4cc52732f7d2cafd7f40d8
kobezzza <kobezzza@gmail.com>
2015-04-27 08:36:23 +0300

    Working on support for sourcemaps

--- monic.js
+++ monic.js
@@ -19,39 +19,60 @@ exports.VERSION = [1, 2, 0];
  * Builds a file
  *
  * @param {string} file - the file path
- * @param {{
- *   flags: (Object|undefined),
- *   labels: (Object|undefined),
- *   content: (?string|undefined),
- *   lineSeparator: (?string|undefined),
- *   replacers: (Array|undefined),
- *   sourceMaps: (?boolean|undefined)
- * }} [params] - additional parameters:
- *
- *   *) [params.flags] - a map of flags;
- *   *) [params.labels] - a map of labels;
- *   *) [params.content] - the file text;
- *   *) [params.lineSeparator] - EOL symbol;
- *   *) [params.replacers] - an array of transform functions;
- *   *) [params.sourceMaps] - if is true, then will be enabled support for source maps.
- *
- * @param {function(Error, string=, string=)} callback - a callback function
+ * @param {Object} params - additional parameters
+ * @param {Object=} [params.flags] - a map of flags
+ * @param {Object=} [params.labels] - a map of labels
+ * @param {?string} [params.content] - the file text
+ * @param {?string} [params.lineSeparator] - EOL symbol
+ * @param {Array=} [params.replacers] - an array of transform functions
+ * @param {?boolean} [params.sourceMaps] - if is true, then will be generated a source map
+ * @param {?string} [params.sourceMapName] - a filename of the source map
+ * @param {?string} [params.sourceFileName] - a filename of the generated file that this source map is associated with
+ * @param {?string} [params.sourceRoot] - a root for all relative URLs in this source map
+ * @param {function(Error, string=, string=, SourceMapGenerator=)} callback - a callback function
  */
 exports.compile = function (file, params, callback) {
 	params = params || {};
+
 	params.flags = params.flags || {};
 	params.labels = params.labels || {};
 	params.lineSeparator = params.lineSeparator || '\n';
 	params.replacers = params.replacers || [];
 	params.sourceMaps = Boolean(params.sourceMaps);
 
+	file = url(file);
+	params.sourceMapName = params.sourceMapName && url(params.sourceMapName);
+	params.sourceFileName = params.sourceFileName ?
+		url(params.sourceFileName) : file;
+
 	function finish(err, fileStructure, path) {
 		if (err) {
 			return callback(err);
 		}
 
-		var map = params.sourceMaps ? new SourceMapGenerator() : null;
-		callback(null, fileStructure.compile(params.labels, params.flags, map), path);
+		var map = params.sourceMaps ?
+			new SourceMapGenerator({
+				file: params.sourceFileName,
+				sourceRoot: params.sourceRoot
+			}) : null;
+
+		var result = fileStructure.compile(params.labels, params.flags, map);
+
+		if (params.sourceMapName) {
+			fs.writeFile(params.sourceMapName, String(map), function (err) {
+				callback(err, result, path, map);
+			});
+
+		} else {
+			callback(null, result, path, map);
+		}
+	}
+
+	function url(url) {
+		return path.normalize(
+			path.resolve(module.parent ?
+				path.dirname(module.parent.filename) : '', url)
+		);
 	}
 
 	var parser = new Parser({
@@ -65,11 +86,6 @@ exports.compile = function (file, params, callback) {
 	Parser.tmpDiff = 0;
 	Parser.diffMap = {};
 	Parser.current = null;
-
-	file = path.normalize(
-		path.resolve(module.parent ?
-			path.dirname(module.parent.filename) : '', file)
-	);
 
 	if (params.content != null) {
 		parser.normalizePath(file, function (err, file) {
