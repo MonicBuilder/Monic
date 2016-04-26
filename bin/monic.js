203fb47df5cdeb1f3c1ada7e844f38791335ba81
kobezzza <kobezzza@gmail.com>
2016-04-26 22:17:50 +0300

    :wrench:

--- bin/monic.js
+++ bin/monic.js
@@ -20,18 +20,15 @@ var
 program
 	.version(monic.VERSION.join('.'))
 	.usage('[options] [file ...]')
-
-	.option('-f, --file [string]', 'path to the source file (meta-information)')
-	.option('-o, --output [string]', 'path to save the generated file')
-	.option('-m, --mode [string]', 'mode for any folders that need to be created for the output folder')
-
-	.option('--eol [char]', 'EOL symbol')
-	.option('--flags [list]', 'list of Monic flags separated by commas')
-	.option('--labels [list]', 'list of Monic labels separated by commas')
+	.option('-f, --file [string]', 'Set a path to the source file (meta-information)')
+	.option('-o, --output [string]', 'Set a path to save the generated file')
+	.option('-m, --mode [string]', 'Set a mode for any folders that need to be created for the output file')
+	.option('--eol [char]', 'Set EOL symbol')
+	.option('--flags [list]', 'Set a list of flags separated by commas')
+	.option('--labels [list]', 'Set a list of labels separated by commas')
 	.option('-s, --source-maps [string]', '[true|false|inline]')
-	.option('--source-map-file [string]', 'path to save the generated source map')
-	.option('--source-root [string]', 'root for all URLs in the generated source map')
-
+	.option('--source-map-file [string]', 'Set a path to save the generated source map')
+	.option('--source-root [string]', 'Set the root for all URLs in the generated source map')
 	.parse(process.argv);
 
 var
@@ -115,6 +112,7 @@ function action(file, input) {
 		flags: (program['flags'] || '').split(',').reduce(toObj, {}),
 		labels: (program['labels'] || '').split(',').reduce(toObj, {}),
 		file: out,
+		mode: program['mode'],
 		sourceMaps: parse(program['sourceMaps']),
 		sourceMapFile: program['sourceMapFile'] || (out || file) + '.map',
 		sourceRoot: program['sourceRoot']
