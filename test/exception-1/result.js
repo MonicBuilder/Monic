ad6395e476a59d315fbc65c32f6f6a1037d328d0
kobezzza <kobezzza@gmail.com>
2021-07-10 12:32:34 +0300

    Added a new test case

---
+++ test/exception-1/result.js
@@ -0,0 +1,19 @@
+/**
+ * ### .match(re[, msg])
+ *
+ * Asserts that the target matches the given regular expression `re`.
+ *
+ *     expect('foobar').to.match(/^foo/);
+ */
+alert('ie = 7');
+
+alert('ie != 8');
+
+
+alert('ie > 6');
+
+alert('ie >= 7');
+
+alert('ie < 8');
+
+alert('ie <= 7');
---
+++ test/exception-1/test.js
@@ -0,0 +1,36 @@
+/**
+ * ### .match(re[, msg])
+ *
+ * Asserts that the target matches the given regular expression `re`.
+ *
+ *     expect('foobar').to.match(/^foo/);
+ */
+//#set ie 7
+//#if ie = 7
+alert('ie = 7');
+//#end if
+
+//#if ie != 8
+alert('ie != 8');
+//#end if
+
+//#if ie != 7
+alert('ie != 7');
+//#end if
+
+//#if ie > 6
+alert('ie > 6');
+//#end if
+
+//#if ie >= 7
+alert('ie >= 7');
+//#end if
+
+//#if ie < 8
+alert('ie < 8');
+//#end if
+
+//#if ie <= 7
+alert('ie <= 7');
+//#end if
+
