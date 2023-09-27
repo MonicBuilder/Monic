a01d5258b131e79088ad01e84e8251c6d4c72d34
kholstinin <kholstinin@yandex-team.ru>
2023-09-27 16:05:29 +0500

    fix: header

--- nyc.config.js
+++ nyc.config.js
@@ -1,3 +1,11 @@
+/*!
+ * Monic
+ * https://github.com/MonicBuilder/Monic
+ *
+ * Released under the MIT license
+ * https://github.com/MonicBuilder/Monic/blob/master/LICENSE
+ */
+
 module.exports = {
 	include: ['dist/*.js']
 }
